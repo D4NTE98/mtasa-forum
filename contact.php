1e4d7a9be9d4bedbb0733a108f15c177a5a04a95
D4NTE <111151866+D4NTE98@users.noreply.github.com>
2026-03-01 15:39:23 +0100

    Update contact.php

--- contact.php
+++ contact.php
@@ -7,7 +7,7 @@ require __DIR__ . '/includes/header.php';
 <section class="section">
   <h1 class="page-title">Kontakt</h1>
   <div class="panel">
-    <div class="empty">Podstrona przykładowa. Tutaj dodaj formularz kontaktowy lub dane.</div>
+    <div class="empty">lorem ipsum</div>
   </div>
 </section>
 
